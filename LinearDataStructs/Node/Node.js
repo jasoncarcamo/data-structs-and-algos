58063927d81ace0092d6053a842b96708dc23cbe
jasoncarcamo <47464765+jasoncarcamo@users.noreply.github.com>
2020-07-28 07:28:45 -0400

    Edited linked list class

---
+++ LinearDataStructs/Node/Node.js
@@ -0,0 +1,23 @@
+class Node { 
+    constructor(data){
+        this.data = data;
+        this.previous = null;
+        this.next = null;
+    };
+
+    setNextNode(data){
+
+        if(!(data instanceof Node)){
+            throw new Error("Data must be of Node type")
+        };
+
+        this.next = data;
+    };
+
+    getNextNode(){
+
+        return this.next;
+    }
+};
+
+module.exports = Node;
